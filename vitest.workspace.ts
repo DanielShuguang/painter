e784342dc932bc1167de9ed9445948038f9a4b4f
DanielShuguang <41940314+DanielShuguang@users.noreply.github.com>
2023-06-27 14:42:11 +0800

    chore: 支持端到端测试

---
+++ vitest.workspace.ts
@@ -0,0 +1,15 @@
+import { defineWorkspace } from 'vitest/config'
+
+export default defineWorkspace([
+  'src/*',
+  {
+    // add "extends" to merge two configs together
+    extends: './vite.config.ts',
+    test: {
+      include: ['test/unit/**/*.{test,spec}.{ts,js,tsx}'],
+      // it is recommended to define a name when using inline configs
+      name: 'jsdom',
+      environment: 'jsdom'
+    }
+  }
+])
